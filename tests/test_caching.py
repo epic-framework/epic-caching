675963ec45da732a38f3cafc4c147c96cee9f51a
Yonatan Perry <yonatan.perry@gmail.com>
2025-06-11 14:33:13 -0400

    add a test that fails on async function caching (reproduces #2)

--- tests/test_caching.py
+++ tests/test_caching.py
@@ -224,3 +224,19 @@ def test_lazy_property():
     value = obj.prop
     assert value is not None
     assert obj.prop is value
+
+
+async def test_async():
+    started = False
+    @cached
+    async def func():
+        nonlocal started
+        started = True
+        return random.random()
+    assert not started
+    f1 = func()
+    assert not started
+    a = await f1
+    assert started
+    b = await func()
+    assert a == b
